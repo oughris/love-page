64c99b52089adefe204db0c7fa5e7a9b15b364a5
oughriss khadija <165613124+oughris@users.noreply.github.com>
2026-02-02 19:53:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,13 @@ const noBtn = document.querySelector(".no-btn");
 const container = document.querySelector(".btn-group");
 const yesBtn = document.querySelector(".yes-btn");
 
-noBtn.addEventListener("mouseover", () => {
-  const containerRect = container.getBoundingClientRect();
-  const btnRect = noBtn.getBoundingClientRect();
-
-  const maxX = containerRect.width - btnRect.width;
-  const maxY = containerRect.height - btnRect.height;
+noBtn.addEventListener("mouseenter", () => {
+  const maxX = container.clientWidth - noBtn.offsetWidth;
+  const maxY = container.clientHeight - noBtn.offsetHeight;
 
   const x = Math.random() * maxX;
   const y = Math.random() * maxY;
 
-  noBtn.style.position = "absolute";
   noBtn.style.left = `${x}px`;
   noBtn.style.top = `${y}px`;
 });
